1106348f1b1aff9e47402832d1868a983c5edf6a
shadow3aaa <shadow3aaa@qq.com>
2024-01-31 22:42:08 +0800

    添加许可证

--- callbacks.lua
+++ callbacks.lua
@@ -1,110 +1,17 @@
 --
--- 中文:
--- 该枚举对应的lua函数
--- 在 fas-rs 中将被 fas-rs 回调
--- pub enum CallBacks {
---  LoadFas(pid_t), --------> function load_fas(pid)
---  UnloadFas(pid_t), ------> function unload_fas(pid)
---  StartFas, --------------> function start_fas()
---  StopFas, ---------------> function stop_fas()
---  InitCpuFreq, -----------> function init_cpu_freq()
---  WriteCpuFreq(usize), ---> function write_cpu_freq(freq)
---  ResetCpuFreq, ----------> function reset_cpu_freq()
--- }
+-- Copyright 2023 shadow3aaa@gitbub.com
 --
--- 可注册的函数说明:
--- function load_fas(pid)
--- 当 fas-rs 加载到目标游戏时调用，
--- 参数为目标应用程序的pid。
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
 --
--- function unload_fas(pid)
--- 当 fas-rs 卸载到目标游戏时调用，
--- 参数为目标应用程序的pid。
+--     http://www.apache.org/licenses/LICENSE-2.0
 --
--- function start_fas()
--- 切换到fas-rs工作状态时调用。
---
--- function stop_fas()
--- 切换到 fas-rs 不工作状态时调用。
---
--- function init_cpu_freq()
--- 当cpu控制器进入控制状态时调用。
---
--- function write_cpu_freq(freq)
--- 当cpu控制器写入cpu频率时调用，
--- 参数是正在写入的频率。
---
--- function reset_cpu_freq()
--- 当cpu控制器退出控制状态时调用。
---
--- 附加: 在函数外的lua代码会在插件加载时被执行，
--- 如果你有执行初始化内容的需求，这样做很方便。
---
--- fas-rs提供的函数:
--- log_info("message")
--- 打印一个info等级日志到/sdcard/Android/fas-rs/fas_log.txt
---
--- log_error("message")
--- 打印一个error等级日志到/sdcard/Android/fas-rs/fas_log.txt
---
--- log_debug("message")
--- 打印一个debug等级日志到/sdcard/Android/fas-rs/fas_log.txt，
--- 此等级在fas-rs的release build不开启
---
--- ------------------------------------
---
--- EN:
--- The lua function corresponding to this enumeration
--- in fas-rs will be called back by fas-rs
--- pub enum CallBacks {
---  LoadFas(pid_t), --------> function load_fas(pid)
---  UnloadFas(pid_t), ------> function unload_fas(pid)
---  StartFas, --------------> function start_fas()
---  StopFas, ---------------> function stop_fas()
---  InitCpuFreq, -----------> function init_cpu_freq()
---  WriteCpuFreq(usize), ---> function write_cpu_freq(freq)
---  ResetCpuFreq, ----------> function reset_cpu_freq()
--- }
---
--- Registerable function description:
--- function load_fas(pid)
--- Called when fas-rs is loaded into the target game,
--- the parameter is the pid of the target application.
---
--- function unload_fas(pid)
--- Called when fas-rs is unloaded into the target game,
--- the parameter is the pid of the target application.
---
--- function start_fas()
--- Called when switching to fas-rs working state.
---
--- function stop_fas()
--- Called when switching to fas-rs not-working state.
---
--- function init_cpu_freq()
--- Called when the cpu controller enters the control state.
---
--- function write_cpu_freq(freq)
--- Called when the cpu controller writes the cpu frequency,
--- the parameter is the frequency being written.
--- 
--- function reset_cpu_freq()
--- Called when the cpu controller exits the control state.
---
--- Extra: Lua code outside the function will be
--- executed when the extension is loaded, if you need to
--- execute initialization content, this is convenient.
---
--- Functions provided by fas-rs:
--- log_info("message")
--- Print an info level log to /sdcard/Android/fas-rs/fas_log.txt
---
--- log_error("message")
--- Print an error level log to /sdcard/Android/fas-rs/fas_log.txt
---
--- log_debug("message")
--- Print a debug level log to /sdcard/Android/fas-rs/fas_log.txt,
--- This level is not enabled in the release build of fas-rs.
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 --
 function init()
     local freqs = io.open("/sys/devices/system/cpu/cpufreq/policy0/scaling_available_frequencies", "r")
