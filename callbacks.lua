cef71dc11f40e09c43ff55ea948f7b1940a2042f
shadow3aaa <shadow3aaa@qq.com>
2024-01-31 22:37:18 +0800

    🐷

--- callbacks.lua
+++ callbacks.lua
@@ -106,9 +106,78 @@
 -- Print a debug level log to /sdcard/Android/fas-rs/fas_log.txt,
 -- This level is not enabled in the release build of fas-rs.
 --
+function init()
+    local freqs = io.open("/sys/devices/system/cpu/cpufreq/policy0/scaling_available_frequencies", "r")
+    max_freq_0 = freqs:read("n*")
 
-log_info("hello world") -- Initial
+    while (true) do
+        local freq = freqs:read("n*")
+        if (freq ~= nil) then
+            min_freq_0 = freq
+        else
+            freqs:close()
+            break
+        end
+    end
 
-function start_fas() -- A example for callback functions
-    log_info("fas started")
+    local freqs = io.open("/sys/devices/system/cpu/cpufreq/policy4/scaling_available_frequencies", "r")
+    max_freq_4 = freqs:read("n*")
+
+    while (true) do
+        local freq = freqs:read("n*")
+        if (freq ~= nil) then
+            min_freq_4 = freq
+        else
+            freqs:close()
+            break
+        end
+    end
+
+    local freqs = io.open("/sys/devices/system/cpu/cpufreq/policy7/scaling_available_frequencies", "r")
+    max_freq_7 = freqs:read("n*")
+
+    while (true) do
+        local freq = freqs:read("n*")
+        if (freq ~= nil) then
+            min_freq_7 = freq
+        else
+            freqs:close()
+            break
+        end
+    end
+end
+
+function bound(freq)
+    io.open("/proc/cpudvfs/cpufreq_debug", "w"):write(string.format("0 %d %d", min_freq_0, freq)):close()
+    io.open("/proc/cpudvfs/cpufreq_debug", "w"):write(string.format("4 %d %d", min_freq_4, freq)):close()
+    io.open("/proc/cpudvfs/cpufreq_debug", "w"):write(string.format("7 %d %d", min_freq_7, freq)):close()
+end
+
+function reset()
+    io.open("/proc/cpudvfs/cpufreq_debug", "w"):write(string.format("0 %d %d", min_freq_0, max_freq_0)):close()
+    io.open("/proc/cpudvfs/cpufreq_debug", "w"):write(string.format("4 %d %d", min_freq_4, max_freq_4)):close()
+    io.open("/proc/cpudvfs/cpufreq_debug", "w"):write(string.format("7 %d %d", min_freq_7, max_freq_7)):close()
+end
+
+function set_governor()
+    io.open("/sys/devices/system/cpu/cpufreq/policy0/scaling_governor", "w"):write("sugov_ext"):close()
+    io.open("/sys/devices/system/cpu/cpufreq/policy4/scaling_governor", "w"):write("schedutil"):close()
+    io.open("/sys/devices/system/cpu/cpufreq/policy7/scaling_governor", "w"):write("schedutil"):close()
+end
+
+init()
+reset()
+set_governor()
+
+function init_cpu_freq()
+    pcall(reset)
+end
+
+function reset_cpu_freq()
+    pcall(reset)
+    pcall(set_governor)
+end
+
+function write_cpu_freq(freq)
+    pcall(bound, freq)
 end
